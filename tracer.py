a5950fa7be0737d10cfc6221cf17ef61c2dc9b5b
Michal Young <michal.young@gmail.com>
2024-10-06 12:28:25 -0700

    New exception if we recognize the developer wrote @trace instead of @trace() .  Also special characters look a little nicer in the indented text.

--- tracer.py
+++ tracer.py
@@ -15,7 +15,12 @@ call-return structure on a single stack.  For example, may be a mess with genera
 
 import sys
 from functools import wraps
+from io import IOBase
 
+ARG_REP_LIMIT = 10  # Limit length of representation of any single argument
+
+class DecorationSyntaxError(NameError):
+    pass
 
 def trace(log_to=sys.stderr, call_stack = []):
     """Use as @trace() or @trace(SEPARATE_STACK).
@@ -24,6 +29,10 @@ def trace(log_to=sys.stderr, call_stack = []):
     Note call_stack will be aliased in traces of different functions
     (intentionally!) unless a different output stream is specified.
     """
+    # Check for user accidentally writing @trace instead of @trace()
+    if not isinstance(log_to, IOBase):
+        raise DecorationSyntaxError("@trace should be @trace() with parentheses")
+
     def trace_wrapper(func):
         # FIXME: Should we associate a different call-stack with each output destination?
         @wraps(func)
@@ -31,22 +40,44 @@ def trace(log_to=sys.stderr, call_stack = []):
             nonlocal call_stack
             nonlocal log_to
             level = len(call_stack)
-            arg_vals = ", ".join(str(arg) for arg in args)
+            arg_vals = ", ".join(arg_str(arg) for arg in args)
             called_like = f"{func.__name__}({arg_vals})"
             if level > 0:
-                print("|   " * (level - 1) + "|---", file=log_to, end="")
+                print("│   " * (level - 1) + "├───", file=log_to, end="")
             print(f"{called_like}", file=log_to)
             call_stack.append(called_like)  # On entry
             retval = func(*args, **kwargs)
             call_stack.pop() # On exit
+            # Omit printing "None" as a return value
+            if retval is None:
+                return None
+            # Otherwise we print the return value
             if level > 0:
-                print("|   " * level, file=log_to, end="")
+                print("│   " * level, file=log_to, end="")
             print(f"{retval}", file=log_to)
             return retval
         return trace_guts
     return trace_wrapper
 
+def arg_str(arg: object) -> str:
+    """Represent argument succinctly, abbreviating if necessary"""
+    raw = str(arg)
+    # Is it too long?
+    if len(raw) > ARG_REP_LIMIT:
+        return "_"
+    return raw
 
+
+# With a single call, tracing fact produces a
+# display like this:
+# fact(4)
+# ├───fact(3)
+# │   ├───fact(2)
+# │   │   ├───fact(1)
+# │   │   │   1
+# │   │   2
+# │   6
+# 24
 @trace()
 def fact(n: int) -> int:
     if n > 1:
@@ -54,11 +85,50 @@ def fact(n: int) -> int:
     else:
         return 1
 
+# GCD follows a similar indentation pattern to
+# fact, but it's a true tail-call, returning a result
+# unchanged all the way up the call chain:
+# gcd(18, 24)
+# ├───gcd(24, 18)
+# │   ├───gcd(18, 6)
+# │   │   ├───gcd(6, 0)
+# │   │   │   6
+# │   │   6
+# │   6
+# 6
 @trace()
 def gcd(m: int, n: int) -> int:
     if n > 0:
         return gcd(n, m % n)
     return m
+
+
+# With fibonacci we can see a more typical example
+# that is not simply tail-recursive, but has multiple
+# recursive calls.  The idea is to make it easy to see how
+# results of calls are combined in the recursive case.  We
+# can also see how redundant these calls are, suggesting that
+# a memoized or dynamic programming implementation would be
+# much more efficient:
+#
+# fib(4)
+# ├───fib(3)
+# │   ├───fib(2)
+# │   │   ├───fib(1)
+# │   │   │   1
+# │   │   ├───fib(0)
+# │   │   │   0
+# │   │   1
+# │   ├───fib(1)
+# │   │   1
+# │   2
+# ├───fib(2)
+# │   ├───fib(1)
+# │   │   1
+# │   ├───fib(0)
+# │   │   0
+# │   1
+# 3
 
 @trace()
 def fib(m: int) -> int:
@@ -67,10 +137,17 @@ def fib(m: int) -> int:
     else:
         return m
 
-# print(fact(4))
-# print()
-# print(gcd(18,24))
-# print()
+# Test of recognizing incorrect syntax in decoration
+# @trace
+# def fib(m: int) -> int:
+#     if m > 1:
+#         return fib(m-1) + fib(m-2)
+#     else:
+#         return m
+
+
 if __name__ == "__main__":
+    print("Tracing factorial(4)")
+    print(fact(4))
     print("Tracing recursive fibonacci(4)")
     print(fib(4))
